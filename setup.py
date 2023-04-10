350438a68f3228fd12c50f84d50e6e288ab585fb
Supratik Chatterjee <supratikdevm96@gmail.com>
2023-04-10 16:17:10 +0530

    Updated for new release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requirements_noversion = [
 setup(
 	# Meta information
 	name				= 'deg',
-	version				= '0.1.0',
+	version				= '1.0.0',
 	author				= 'Supratik Chatterjee',
 	author_email			= 'supratikdevm96@gmail.com',
 	url				= 'https://github.com/supratikchatterjee16/deg',
